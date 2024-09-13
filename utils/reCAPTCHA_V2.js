e0bc629018bdc069a312ab69a0b63423e51d489e
IronWalrus68 <camsjobhunting@gmail.com>
2024-09-13 22:26:01 +0100

    add reCAPTCH util, update email route to use new util

--- utils/reCAPTCHA_V2.js
+++ utils/reCAPTCHA_V2.js
@@ -1,1 +1,20 @@
-require('dotenv').config();
+require('dotenv').config(); 
+
+    let reCAPTCHA = async function (response_key){
+        const secret_key = process.env.recaptchaSecret;
+
+        const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${response_key}`;
+
+        // Making POST request to verify captcha
+        const google_response = await fetch(url, { method: "post" }).then(response => response.json());
+
+        if (google_response.success) {
+            // Captcha is verified
+            return;
+        } else {
+            // If captcha is not verified
+            console.error("Failed to verify reCAPTCHA.");
+            throw error;
+        }
+    } // end of reCAPTCHA func
+module.exports = reCAPTCHA;
